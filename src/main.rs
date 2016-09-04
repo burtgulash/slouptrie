6640fdf27f1ec78f559099d384ad0b6af7156c78
TM <none@none>
2016-09-04 21:01:13 +0200

    Implement search fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::iter;
+use std::{iter,str};
+use std::cmp::min;
 
 struct TNode<'a> {
     prefix_size: usize,
@@ -26,6 +27,17 @@ fn common_prefix_size(a: &str, b: &str) -> usize {
         .fold(0, |acc, (x, _)| acc + x.len_utf8())
 }
 
+fn cum_encode(xs: &[usize]) -> Vec<usize> {
+    let mut cum = 0usize;
+    let mut res = Vec::new();
+    for x in xs {
+        res.push(cum);
+        cum += *x;
+    }
+    res.push(cum);
+    res
+}
+
 struct Trie {
     terms: Vec<u8>,
     firsts: Vec<char>,
@@ -46,7 +58,11 @@ impl Trie {
             children: Vec::new(),
             is_terminal: Vec::new(),
         };
+
         t.build(words);
+        t.termlens = cum_encode(&t.termlens);
+        t.nodes = cum_encode(&t.nodes);
+
         t
     }
 
@@ -105,6 +121,48 @@ impl Trie {
         }
         self.nodes.push(root.children.len());
     }
+
+    fn search(&self, mut word: &str, search_prefix: bool) -> bool {
+        let nl = self.nodes.len();
+        let (mut ch0, mut ch1) = (self.nodes[nl - 2], self.nodes[nl - 1]);
+        loop {
+            let c = word.chars().next().unwrap();
+            let ci = ch0 + match self.firsts[ch0 .. ch1].binary_search(&c) {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+
+            // println!("CI: {}, ch0: {}, ch1: {}", ci, ch0, ch1);
+            let (t0, t1) = (self.termlens[ci], self.termlens[ci + 1]);
+            let matched = str::from_utf8(&self.terms[t0 .. t1]).unwrap();
+
+            //println!("matched len: {},matched: {}", matched.len(), matched);
+            //println!("WORD: {}, matched: {}", &word[.. min(word.len(), matched.len())], matched);
+
+            if word.len() < matched.len() {
+                if word != &matched[..word.len()] {
+                    return false;
+                }
+                return search_prefix;
+            } else if word.len() == matched.len() {
+                if word != matched {
+                    return false;
+                }
+                return search_prefix || self.is_terminal[ci];
+            }
+
+            word = &word[matched.len() ..];
+            let node = self.children[ci];
+            if node == 0 {
+                return false;
+            }
+
+            ch0 = self.nodes[node];
+            ch1 = self.nodes[node + 1];
+        }
+
+        false
+    }
 }
 
 fn main() {
@@ -143,11 +201,18 @@ let mut ws = vec![
 ];
 
     let mut words = vec!["auto", "autobus", "brno", "brnena", "autori"];
+    //ws = words;
     let t = Trie::new(&mut ws);
     println!("{:?}", t.is_terminal);
     println!("{:?}", t.termlens);
-    println!("{:?}", t.firsts);
-    println!("{:?}", t.terms);
+    println!("{:?}", t.firsts.iter().cloned().collect::<String>());
+    println!("{:?}", str::from_utf8(&t.terms[..]).unwrap());
     println!("{:?}", t.nodes);
     println!("{:?}", t.children);
+
+    println!("SEARCHING");
+    let prefix_search = false;
+    for w in ws {
+        println!("SEARCH(prefix={:?}) {}: {}", prefix_search, w, t.search(&w[..min(w.len(),3)], prefix_search));
+    }
 }
